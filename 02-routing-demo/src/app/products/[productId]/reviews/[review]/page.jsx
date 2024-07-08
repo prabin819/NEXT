f3a36a9cf499deefc37fd4bed3da4599a8cfb9de
Prabin <manandharprabin819@gmail.com>
2024-07-08 10:30:59 +0545

    not found page (and its hierarchy)

--- 02-routing-demo/src/app/products/[productId]/reviews/[review]/page.jsx
+++ 02-routing-demo/src/app/products/[productId]/reviews/[review]/page.jsx
@@ -1,6 +1,12 @@
+import { notFound } from "next/navigation";
+
 export default function Review({ params }) {
   //params available as a prop
   //params object contains route parameters as key value pairs
+
+  if (parseInt(params.review) > 1000) {
+    notFound();
+  }
   return (
     <>
       <h1>
