90280553b3757c6db881d92b45e83fd1bcd24b2b
Prabin <manandharprabin819@gmail.com>
2024-07-08 14:35:50 +0545

    17-metadata, its hierarchy and dynamic metadata with async/await

--- 02-routing-demo/src/app/about/page.jsx
+++ 02-routing-demo/src/app/about/page.jsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "about code evolution",
+};
+
 export default function About() {
   return <h1>About me!</h1>;
 }
--- 02-routing-demo/src/app/products/[productId]/page.jsx
+++ 02-routing-demo/src/app/products/[productId]/page.jsx
@@ -1,4 +1,31 @@
-export default function ProductList({ params }) {
+// import {Metadata} from "next";
+
+// type Props = {
+//   params: {
+//     productId: string;
+//   }
+// }
+
+// export const generateMetadata = ({params: Props}): Metadata => {
+// return {
+//   title: `Product ${params.productId}`
+// }
+// }
+
+//typescript code
+
+export const generateMetadata = async ({ params }) => {
+  const title = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }) {
   //params available as a prop
   //params object contains route parameters as key value pairs
   return (
