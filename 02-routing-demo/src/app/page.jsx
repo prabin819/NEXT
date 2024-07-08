dde04401cb85decf4b5cda9f1ff36ccb3f7f47c8
Prabin <manandharprabin819@gmail.com>
2024-07-08 16:55:10 +0545

    19-Link component navigation

--- 02-routing-demo/src/app/page.jsx
+++ 02-routing-demo/src/app/page.jsx
@@ -1,4 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome to Home Page</h1>;
+  return (
+    <>
+      <h1>Welcome to Home Page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </>
+  );
 }
 // i am using js instead of ts in this project.
