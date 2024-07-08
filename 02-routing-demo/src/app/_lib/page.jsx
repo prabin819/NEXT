59c0abf418cbaa340d5819a91b6853a79e64419a
Prabin <manandharprabin819@gmail.com>
2024-07-08 10:47:41 +0545

    private folders

---
+++ 02-routing-demo/src/app/_lib/page.jsx
@@ -0,0 +1,11 @@
+function PrivateRoute() {
+  return (
+    <>
+      <h1>
+        You cannot view tihs in the browser because this is inside a private
+        folder.
+      </h1>
+      ;<p>and private folder name start with underscore</p>
+    </>
+  );
+}
