0f986569489c833e56bc141a2a5de6adcc5547bc
Prabin <manandharprabin819@gmail.com>
2024-07-08 12:22:02 +0545

    14-Layouts exercise

---
+++ 02-routing-demo/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div
+      style={{
+        backgroundColor: "ghostwhite",
+        padding: "1rem",
+      }}
+    >
+      <h1>Footer</h1>
+    </div>
+  );
+}
---
+++ 02-routing-demo/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div
+      style={{
+        backgroundColor: "lightblue",
+        padding: "1rem",
+      }}
+    >
+      <h1>Header</h1>
+    </div>
+  );
+}
