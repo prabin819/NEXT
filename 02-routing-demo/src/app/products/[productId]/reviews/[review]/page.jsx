08a58651408ac3f6328991300bd89a5c62e5a9db
Prabin <manandharprabin819@gmail.com>
2024-07-09 12:24:57 +0545

    24-error handling

--- 02-routing-demo/src/app/products/[productId]/reviews/[review]/page.jsx
+++ 02-routing-demo/src/app/products/[productId]/reviews/[review]/page.jsx
@@ -1,8 +1,19 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count) {
+  return Math.floor(Math.random() * count);
+  //returns rand num between 0 and count (0 included, count not included)
+}
+
 export default function Review({ params }) {
   //params available as a prop
   //params object contains route parameters as key value pairs
+
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review.");
+  }
 
   if (parseInt(params.review) > 1000) {
     notFound();
