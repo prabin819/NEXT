ce861fe46d7dce1a83d1616aad5ae77acf8c8b1b
Prabin <manandharprabin819@gmail.com>
2024-07-10 11:56:51 +0545

    27-handling errors in layouts (the error boundary will not throw errors thrown in the layout.tsx component WITHIN THE SAME SEGMENT)

--- 02-routing-demo/src/app/products/[productId]/layout.jsx
+++ 02-routing-demo/src/app/products/[productId]/layout.jsx
@@ -1,3 +1,12 @@
+function getRandomInt(count) {
+  return Math.floor(Math.random() * count);
+  //returns rand num between 0 and count (0 included, count not included)
+}
+const random = getRandomInt(2);
+
+if (random === 1) {
+  throw new Error("Error loading product.");
+}
 export default function ProductDetailsLayout({ params, children }) {
   return (
     <div>
--- 02-routing-demo/src/app/products/[productId]/reviews/[review]/page.jsx
+++ 02-routing-demo/src/app/products/[productId]/reviews/[review]/page.jsx
@@ -10,11 +10,11 @@ export default function Review({ params }) {
   //params available as a prop
   //params object contains route parameters as key value pairs
 
-  const random = getRandomInt(2);
+  // const random = getRandomInt(2);
 
-  if (random === 1) {
-    throw new Error("Error loading review.");
-  }
+  // if (random === 1) {
+  //   throw new Error("Error loading review.");
+  // }
 
   if (parseInt(params.review) > 1000) {
     notFound();
