4717a51dc60afc59666251e433c1f3e30c687ad4
Prabin <manandharprabin819@gmail.com>
2024-07-10 23:46:44 +0545

    40 => Redirects in Route Handlers

--- 33-route-handlers/src/app/comments/[id]/route.ts
+++ 33-route-handlers/src/app/comments/[id]/route.ts
@@ -1,9 +1,13 @@
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async function GET(
   _request: Request,
   { params }: { params: { id: string } } //id corresponds to the folder name
 ) {
+  if (parseInt(params.id) > comments.length) {
+    redirect("/comments");
+  }
   const comment = comments.find(
     (comment) => comment.id === parseInt(params.id)
   );
