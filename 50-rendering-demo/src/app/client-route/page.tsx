05c413d868a8ebe92d139631903389048864557c
Prabin <manandharprabin819@gmail.com>
2024-07-12 01:51:56 +0545

    56 =>yarn add server-only (server-only code)

---
+++ 50-rendering-demo/src/app/client-route/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { serverSideFunction } from "../utils/server-utils";
+
+export default function ClientRoutePage() {
+  console.log(`client route rendered`);
+
+  const result = serverSideFunction();
+
+  return (
+    <>
+      <h1>ClientRoutePage</h1>
+      <p>{result}</p>
+    </>
+  );
+}
