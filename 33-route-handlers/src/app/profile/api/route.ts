194a23577f136e288cdefb29410e46daed6cd5de
Prabin <manandharprabin819@gmail.com>
2024-07-11 12:57:06 +0545

    41 => Headers in Route Handlers => getting headers

--- 33-route-handlers/src/app/profile/api/route.ts
+++ 33-route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,20 @@
+// method #1
+// import { type NextRequest } from "next/server";
+
+// export async function GET(request: NextRequest) {
+//   const requestHeaders = new Headers(request.headers);
+//   console.log(requestHeaders.get("Authorization"));
+
+//   return new Response("this is profile api");
+// }
+
+// method #2
+import { headers } from "next/headers";
+
 export async function GET() {
+  const headerList = headers();
+
+  console.log(headerList.get("Authorization"));
+
   return new Response("this is profile api");
 }
