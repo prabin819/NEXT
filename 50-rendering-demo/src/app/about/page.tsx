e0ad8b46c6d1b362b33fd09602eaea8b6685f53f
Prabin <manandharprabin819@gmail.com>
2024-07-12 00:56:40 +0545

    52,53,54 => server rendering strategies => STATIC RENDERING, DYNAMIC RENDERING & STREAMING

--- 50-rendering-demo/src/app/about/page.tsx
+++ 50-rendering-demo/src/app/about/page.tsx
@@ -1,9 +1,14 @@
 //import { useState } from "react";
+import { cookies } from "next/headers";
 
 export default function AboutPage() {
-  console.log("about server component");
-
   //const [name, setName] = useState("");
+
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
+
+  console.log("about server component");
   return (
     <div>
       <h1>About Page</h1>
