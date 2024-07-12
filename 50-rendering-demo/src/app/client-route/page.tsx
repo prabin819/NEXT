f88d09d5f91da52aa8cc9b091f08f5be1da3ee92
Prabin <manandharprabin819@gmail.com>
2024-07-12 10:48:19 +0545

    58 => context providers

--- 50-rendering-demo/src/app/client-route/page.tsx
+++ 50-rendering-demo/src/app/client-route/page.tsx
@@ -21,27 +21,40 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/ThemeProvider";
 
 export default function ImageSlider() {
   const settings = {
     dots: true,
   };
+  const theme = useTheme();
   return (
-    <div className="image-slider-container">
-      <Slider {...settings}>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-      </Slider>
-    </div>
+    <>
+      <div className="image-slider-container">
+        <Slider {...settings}>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+          <div>
+            <img src="http://picsum.photos/400/200" />
+          </div>
+        </Slider>
+      </div>
+      <div>
+        <h1
+          style={{
+            color: theme.colors.primary,
+          }}
+        >
+          Client Route
+        </h1>
+      </div>
+    </>
   );
 }
